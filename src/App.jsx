04c3c16b25d7988a8956f022c36c1477d79fe042
keserizzet <izzetfurkankeserwtk@gmail.com>
2025-03-03 01:56:08 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,12 @@ import ContactList from "./components/ContactList/ContactList";
 const App = () => {
   const [contacts, setContacts] = useState(() => {
     const savedContacts = localStorage.getItem("contacts");
-    return savedContacts ? JSON.parse(savedContacts) : [];
+    return savedContacts ? JSON.parse(savedContacts) : [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ];
   });
   const [filter, setFilter] = useState("");
 
